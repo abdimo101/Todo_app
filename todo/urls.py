eb704ba727ce883456d268b299ea6b0e37f7ae03
Abdi <abdi.abdulle@outlook.dk>
2023-09-03 21:29:14 +0200

    first commit

---
+++ todo/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from todo import views
+
+
+app_name = 'todo'
+
+urlpatterns = [
+   path('', views.index, name='index'),
+
+]
---
+++ todo/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from todo.models import Todo
+# Create your views here.
+
+def index(request):
+    todos = Todo.objects.all()
+    context = {'todos': todos }
+    return render(request, 'todo/index.html', context)
